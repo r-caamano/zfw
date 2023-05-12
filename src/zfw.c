a7156585f9ad9da580cdb7bd793430ade407aada
r-caamano <robert.caamano@netfoundry.io>
2023-05-12 23:32:18 +0000

    changed initial release to 0.1.0

--- src/zfw.c
+++ src/zfw.c
@@ -117,7 +117,7 @@ static char *tun_interface;
 static char *tc_interface;
 static char *object_file;
 static char *direction_string;
-const char *argp_program_version = "1.0";
+const char *argp_program_version = "0.1.0";
 
 static __u8 if_list[MAX_IF_LIST_ENTRIES];
 int ifcount = 0;
